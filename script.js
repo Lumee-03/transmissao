412d80aa708d4127e3e9d2886455cd1ddfde4c7f
Lumee-03 <lumee03@gmail.com>
2025-08-05 20:09:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function copiarPix() {
-  const chave = document.getElementById("pix-key").innerText;
-  navigator.clipboard.writeText(chave).then(() => {
-    alert("Chave Pix copiada com sucesso!");
-  });
-}
-
 function entrarEmTelaCheia() {
   const elem = document.getElementById("live-player-container");
   if (elem.requestFullscreen) {
-    elem.requestFullscreen();
+    elem.requestFullscreen().catch(() => {
+      alert("Não foi possível ativar a tela cheia neste dispositivo.");
+    });
   } else if (elem.webkitRequestFullscreen) { // Safari
     elem.webkitRequestFullscreen();
   } else if (elem.msRequestFullscreen) { // IE11
     elem.msRequestFullscreen();
+  } else {
+    alert("Tela cheia não suportada pelo seu navegador.");
   }
 }
